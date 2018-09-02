490c9bfe83fbfe4e3f6c6549e3ddc5ae46e8023d
Ted Barlow <ted@noukod.com>
2018-09-02 23:42:44 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,48 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    user_index = input_to_index(user_input)
+    a_value = "X"
+    valid_move?(board, user_index)
+    move(board, user_index, a_value)
+    display_board(board)
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def move(board, user_index, a_value = "X")
+  board[user_index] = a_value
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    return false
+  else
+    return true
+  end
+  if index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
